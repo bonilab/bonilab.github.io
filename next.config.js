0adae82801b712e5a8048a5b326cfd8729303927
Nguyen Tran <merlinvn@gmail.com>
2020-12-23 19:31:04 -0500

    try to fix assetPrfix with .nojekyll in github action

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/bonilab/bonilab.github.io/gh-pages/' : '',
-}
+  // assetPrefix: isProd ? 'https://cdn.statically.io/gh/bonilab/bonilab.github.io/gh-pages/' : '',
+};
