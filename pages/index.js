d0bc28f11187cecd90ecb12c15a2d19f65609f58
rjzupkoii <rjzii@yahoo.com>
2021-01-11 17:52:12 -0500

    Update index.js
    RM default text

--- pages/index.js
+++ pages/index.js
@@ -251,12 +251,8 @@ const ResearchArea = (props) => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <h2 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            Research Area
+            Research Areas
           </h2>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-            voluptatum cupiditate veritatis in accusamus quisquam.
-          </p>
         </div>
 
         <div className="mt-10">
